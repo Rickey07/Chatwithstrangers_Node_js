87a8bc579dd4f537712ca523abc1881c4bff0a80
Rickey07 <59111961+Rickey07@users.noreply.github.com>
2020-06-12 19:28:11 +0530

    Updated client js with sound feature

--- public/js/client.js
+++ public/js/client.js
@@ -25,7 +25,7 @@ const append = (message, position) => {
 };
 
 // set up the client connection to receive the updates
-var socket = io("http://chatwithstrangers0.herokuapp.com");
+var socket = io("https://chatwithstrangers0.herokuapp.com");
 
 socket.emit("new-connection", userName);
 
